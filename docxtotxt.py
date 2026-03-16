dfcb667420985d2246b19278efc69b3a51801a00
觉 <156421259+iamfromchangsha@users.noreply.github.com>
2026-03-16 21:35:06 +0800

    Update docxtotxt.py

--- docxtotxt.py
+++ docxtotxt.py
@@ -1,15 +1,7 @@
 from docx import Document
 
 def extract_text_from_docx(file_path):
-    """
-    从 .docx 文件中提取所有文本内容。
-
-    参数:
-        file_path (str): DOCX 文件的路径。
-
-    返回:
-        str: 文档中所有段落文本，用换行符 '\n' 连接。
-    """
+    
     try:
         doc = Document(file_path)
         full_text = []
