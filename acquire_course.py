6ff594de1d8d66f32b4bdd69a4f259154405dbae
觉 <156421259+iamfromchangsha@users.noreply.github.com>
2026-03-16 21:16:56 +0800

    Add files via upload

---
+++ acquire_course.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+def get_kecheng(token,num):
+    print(token)
+    headers = {
+        'Authorization': 'Bearer '+token,
+        'Cookie':'WT-prd-rememberme=false; WT-prd-language=zh-CN; WT-prd-access-token='+token+'; WT-prd-login-schoolId=f8d0cbf5d0d5aed8d698612d4212b7a9; WT-prd-refresh-token=5b7472731c074b6a9d4ea4d19ab2b8bb; WT-prd-refresh-token-state-v2=0; WT-prd-teaching-schoolId=0',
+        'Referer':'https://whut.ai-augmented.com',
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
+    }
+    response = requests.get('https://whut.ai-augmented.com/api/jx-iresource/group/student/groups?time_flag='+num, headers=headers)
+    print(response.text)
+    kechengbiao = response.json()
+    return kechengbiao
+
+#get_kecheng('7361737da39f4aa6b945559600faf135','1')
+
