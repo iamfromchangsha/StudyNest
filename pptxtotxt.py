bb7157bac6ed6f7ed8240f419bc320ea2322e2fb
觉 <156421259+iamfromchangsha@users.noreply.github.com>
2026-03-16 22:04:28 +0800

    Update pptxtotxt.py

--- pptxtotxt.py
+++ pptxtotxt.py
@@ -1,15 +1,7 @@
 from pptx import Presentation
 
 def extract_text_from_pptx(file_path):
-    """
-    从 .pptx 文件中提取所有文本内容。
 
-    参数:
-        file_path (str): PPTX 文件的路径。
-
-    返回:
-        str: 所有幻灯片中文本内容拼接后的字符串，每段文本用换行符分隔。
-    """
     try:
         prs = Presentation(file_path)
         text_runs = []
@@ -25,5 +17,3 @@ def extract_text_from_pptx(file_path):
         raise RuntimeError(f"无法读取PPTX文件 {file_path}: {e}")
     
 
-# text = extract_text_from_pptx(r"E:\桌面\whut\深入学习贯彻党的二十届三中全会精神.pptx")
-# print(text)
